945490c4cfb95ad0a61138eb67821ed7c0c1663e
vasabyy98 <roman.vasjkov98@gmail.com>
2022-06-18 15:05:59 +0300

    minor JSX and CSS changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -18,54 +18,49 @@ function Register() {
   return (
     <>
       <section className={styles.content__wrapper}>
-        <div className={styles.form}>
+        <form className={styles.form}>
           <div className={styles.form__group}>
             <header className={styles.form__heading}>
               <span className={styles.heading}>name</span>
             </header>
-            <form>
-              <input
-                type="text"
-                className={styles.form__control}
-                id="name"
-                name="name"
-                value={name}
-                placeholder="type your name here..."
-                onChange={onChange}
-              />
-            </form>
+
+            <input
+              type="text"
+              className={styles.form__control}
+              id="name"
+              name="name"
+              value={name}
+              placeholder="type your name here..."
+              onChange={onChange}
+            />
           </div>
-          <div className={styles.form}>
-            <div className={styles.form__group}>
-              <header className={styles.form__heading}>
-                <span className={styles.heading}>email adress</span>
-              </header>
-              <form>
-                <input
-                  type="email"
-                  className={styles.form__control}
-                  id="email"
-                  name="email"
-                  placeholder="type your email here..."
-                />
-              </form>
-            </div>
-            <div className={styles.form__group}>
-              <header className={styles.form__heading}>
-                <span className={styles.heading}>password</span>
-              </header>
-              <form>
-                <input
-                  type="password"
-                  className={styles.form__control}
-                  id="password"
-                  name="password"
-                  placeholder="type your password here..."
-                />
-              </form>
-            </div>
+          <div className={styles.form__group}>
+            <header className={styles.form__heading}>
+              <span className={styles.heading}>email adress</span>
+            </header>
+
+            <input
+              type="email"
+              className={styles.form__control}
+              id="email"
+              name="email"
+              placeholder="type your email here..."
+            />
           </div>
-        </div>
+          <div className={styles.form__group}>
+            <header className={styles.form__heading}>
+              <span className={styles.heading}>password</span>
+            </header>
+
+            <input
+              type="password"
+              className={styles.form__control}
+              id="password"
+              name="password"
+              placeholder="type your password here..."
+            />
+          </div>
+        </form>
         <div className={`${btnStyles.form__btns} ${btnStyles.absolute}`}>
           <Link to="/login">
             <button
