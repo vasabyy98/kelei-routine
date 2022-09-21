3193afb2930518631d9fa534670e2560d6550cc7
vasabyy98 <roman.vasjkov98@gmail.com>
2022-09-21 22:12:13 +0300

    added splash screens for ios devices

--- frontend/src/components/deviceSizeMessage/DeviceSize.jsx
+++ frontend/src/components/deviceSizeMessage/DeviceSize.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function deviceSize() {
   return (
     <div className="device__width__error">
-      <span>Devices with width over 1200px is not supported!</span>
+      <span>Devices with width over 1200px are not yet supported!</span>
     </div>
   );
 }
